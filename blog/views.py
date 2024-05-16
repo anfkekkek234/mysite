c07c03a59eaa75999507cf18a33128eb24885c01
Ali <awp.828.cr7@email.com>
2024-05-16 20:21:21 +0330

    continue
    continue template tag and practice

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render , get_object_or_404
-
+from datetime import datetime
 from blog.models import Post
 
 
 # Create your views here.
 def blog(request):
-    posts = Post.objects.filter(status=1)
+    current_time = datetime.now()
+    posts = Post.objects.filter(status=1,published_date__lte=current_time)
     context = {'posts': posts}
     return render(request,'blog/blog-home.html',context)
 
 def blog_single(request,pid):
     post = get_object_or_404(Post,pk=pid,status=1)
+    post.counted_views += 1
+    post.save()
     context = {'post':post}
     return render(request, 'blog/blog-single.html',context)
